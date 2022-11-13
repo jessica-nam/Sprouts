b11c21c92db59c5f748825afe570f877ed66fe55
Jessica Nam <jessicanam@nevada.unr.edu>
2022-11-13 13:57:07 -0800

    Place babies on slot 1

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -10,6 +10,12 @@ public class Plant : MonoBehaviour
 
     [SerializeField] private Tilemap interactableMap;
     [SerializeField] private Tile baby1;
+    [SerializeField] private Tile baby2;
+    [SerializeField] private Tile baby3;
+    [SerializeField] private Tile baby4;
+    [SerializeField] private Tile baby5;
+    [SerializeField] private Tile baby6;
+
 
     public bool BabyPlanted = false;
 
@@ -28,6 +34,26 @@ public class Plant : MonoBehaviour
         if(MouseItemData.instance.hasItem){
             if(InventoryDisplay.instance.babyName == "Baby 1"){
                 interactableMap.SetTile(new Vector3Int(-5, -6, 0), baby1);
+                Debug.Log("Plant");
+                BabyPlanted = true;
+            }else if(InventoryDisplay.instance.babyName == "Baby 2"){
+                interactableMap.SetTile(new Vector3Int(-5, -6, 0), baby2);
+                Debug.Log("Plant");
+                BabyPlanted = true;
+            }else if(InventoryDisplay.instance.babyName == "Baby 3"){
+                interactableMap.SetTile(new Vector3Int(-5, -6, 0), baby3);
+                Debug.Log("Plant");
+                BabyPlanted = true;
+            }else if(InventoryDisplay.instance.babyName == "Baby 4"){
+                interactableMap.SetTile(new Vector3Int(-5, -6, 0), baby4);
+                Debug.Log("Plant");
+                BabyPlanted = true;
+            }else if(InventoryDisplay.instance.babyName == "Baby 5"){
+                interactableMap.SetTile(new Vector3Int(-5, -6, 0), baby5);
+                Debug.Log("Plant");
+                BabyPlanted = true;
+            }else if(InventoryDisplay.instance.babyName == "Baby 6"){
+                interactableMap.SetTile(new Vector3Int(-5, -6, 0), baby6);
                 Debug.Log("Plant");
                 BabyPlanted = true;
             }
