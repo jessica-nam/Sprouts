7d590f015b7d62886d1468188cb5e3d9523271c2
Blastimbre <nmcfadden@nevada.unr.edu>
2022-11-14 13:03:45 -0800

    Adding Time Basics
    Basic Time script and objects

---
+++ Assets/Scripts/TimeScripts/TimeManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class TimeManager : MonoBehaviour
+{
+    public int currentDay = 1;
+    public int previousDay = 0;
+    public TMP_Text currDay;
+    SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        currDay.text = "Day: " + currentDay.ToString();
+    }
+
+    // Update is called once per frame
+    private void OnMouseDown()
+    {
+        //bring up canvas to make sure user wants to move to next day
+
+        //test click is working properly
+        previousDay = currentDay;
+        currentDay += 1;
+
+    }
+
+    private void OnMouseOver()
+    {
+        //change sign color when hovered over.
+        sprite.color = new Color(255, 0, 0, 1);
+    }
+    private void OnMouseExit()
+    {
+        sprite.color = new Color(255, 255, 255, 1);
+    }
+}
