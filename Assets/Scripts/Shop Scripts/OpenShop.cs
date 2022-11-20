f3b456a7e65847ac6836803d8c541058f29a52aa
Jessica Nam <jessicanam@nevada.unr.edu>
2022-11-20 09:12:29 -0800

    Disable Next Day when shop open

--- Assets/Scripts/Shop Scripts/OpenShop.cs
+++ Assets/Scripts/Shop Scripts/OpenShop.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenShop : MonoBehaviour
 {
     [SerializeField] Canvas ShopCanvas;
+    [SerializeField] private GameObject nextDay;
     private void OnMouseDown()
     {
         ShopCanvas.gameObject.SetActive(true);
+        nextDay.SetActive(false);
     }
 }
