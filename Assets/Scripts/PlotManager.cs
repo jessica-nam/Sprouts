33a954e576b4cdae163a6c780a87ac2a7025018b
Jessica Nam <jessicanam@nevada.unr.edu>
2022-11-23 01:24:12 -0800

    Bug fix

--- Assets/Scripts/PlotManager.cs
+++ Assets/Scripts/PlotManager.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlotManager : MonoBehaviour
 {
 
+    public static PlotManager instance;
+
     public MouseItemData mouseObj;
     public ShopItemSO babyObj;
-    public ShopItemSO DoneBaby;
     public InventorySlot slotData;
     private GameObject invHolder;
-    
+
+    public Button NextDay;
+
 
     [SerializeField] private Sprite icon;
-    [SerializeField] private string title;
 
-    GameObject SavedObjs; 
+    GameObject SavedObjs;
 
     bool isPlanted = false;
     SpriteRenderer plant;
@@ -25,73 +28,87 @@ public class PlotManager : MonoBehaviour
     float timeBtwStages = 2f;
     float timer;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
         plant = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        NextDay.onClick.AddListener(NextDayButtonYes);
     }
 
-    private void Awake(){
-        SavedObjs = SaveObject.savedObjs; 
+    private void Awake()
+    {
+        instance = this;
+        SavedObjs = SaveObject.savedObjs;
         invHolder = SavedObjs.gameObject.transform.Find("Inventory Holder").gameObject;
         mouseObj = SavedObjs.gameObject.transform.Find("Mouse Object").gameObject.GetComponent<MouseItemData>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(isPlanted){
-            
-                if(TimeManager.instance.clickedYes){
-                    Debug.Log("Clicked Yes");
-                    timer = timeBtwStages;
-                    plantStage++;
-                    UpdatePlant();
-                    TimeManager.instance.clickedYes = false;
-                }else{
-                    Debug.Log("NO");
-                }
+    // void Update()
+    // {
+    //     if (isPlanted)
+    //     {
+    //         timer -= Time.deltaTime;
+
+    //         if (timer < 0 && plantStage < plantStages.Length - 1)
+    //         {
                 
+
+    //         }
+    //     }
+    // }
+
+    public void NextDayButtonYes()
+    {
+        if (plantStage < plantStages.Length - 1){
+            //timer = timeBtwStages;
+            plantStage+=1;
+            UpdatePlant();
         }
+        
     }
 
-    private void OnMouseDown(){
-        if(isPlanted)
+    private void OnMouseDown()
+    {
+        if (isPlanted)
         {
-            if(plantStage == plantStages.Length-1){    
-    
+            if (plantStage == plantStages.Length - 1)
+            {
+
                 Harvest();
 
+            }else{
+                Debug.Log("Stop");
             }
-        }else
+        }
+        else
         {
             Plant();
         }
     }
 
-    void Harvest(){
+    void Harvest()
+    {
 
         var inventory = invHolder.GetComponent<InventoryHolder>();
         var clone = Instantiate(babyObj);
         clone.icon = icon; // refers to serialize field at top
-        clone.title = title;
         inventory.InventorySystem.AddToInventory(clone, 1);
 
         isPlanted = false;
         plant.gameObject.SetActive(false);
-        Debug.Log("Harvest object");
-        Debug.Log(mouseObj.getCurrentMouseItem().ItemData);
-        Debug.Log(DoneBaby);
+
     }
 
-    void Plant(){
-        if(MouseItemData.instance.hasItem){
+    void Plant()
+    {
+        if (MouseItemData.instance.hasItem)
+        {
 
-            babyObj = mouseObj.getCurrentMouseItem().ItemData; 
+            babyObj = mouseObj.getCurrentMouseItem().ItemData;
             Debug.Log("Baby on mouse: ");
-
-            
-            Debug.Log(DoneBaby);
 
             isPlanted = true;
             plantStage = 0;
@@ -103,7 +120,8 @@ public class PlotManager : MonoBehaviour
         }
     }
 
-    void UpdatePlant(){
+    void UpdatePlant()
+    {
         plant.sprite = plantStages[plantStage];
     }
 }
